a8c100564b7d8ecac13ab2a5272a76cd98a4916e
SpockST <167228572+SpockST@users.noreply.github.com>
2024-06-28 19:42:44 +0300

    28.06

--- src/tests/performVerifySorting.spec.js
+++ src/tests/performVerifySorting.spec.js
@@ -4,33 +4,38 @@ const { login, password } = require('../auth/user.json');
 
 test.describe('Inventory Sorting', () => {
     const testParams = [
-        { name: 'az', sort: [] },
-        { name: 'za', sort: [] },
-        { name: 'lohi', sort: [] },
-        { name: 'hilo', sort: [] },
+        { name: ['az', 'Name'] },
+        { name: ['za', 'Name'] },
+        { name: ['lohi', 'Price'] },
+        { name: ['hilo', 'Price'] },
     ];
 
     test.beforeEach(async ({ inventoryPage, loginPage }) => {
         await inventoryPage.navigate();
         await loginPage.performLogin(login, password);
-
-        const itemNames = await inventoryPage.getAllProductNames();
-        testParams[0].sort = [...itemNames].sort();
-        testParams[1].sort = [...itemNames].sort().reverse();
-
-        const itemPrices = await inventoryPage.getAllPrices();
-        testParams[2].sort = [...itemPrices].sort((a, b) => a - b);
-        testParams[3].sort = [...itemPrices].sort((a, b) => b - a);
     });
 
     testParams.forEach((elem) => {
-        test.only(`Sort items by ${elem.name}`, async ({ inventoryPage }) => {
+        test.only(`Sort Products by ${elem.name[1]} ${elem.name[0]}`, async ({ inventoryPage }) => {
             let testData = [];
-            await inventoryPage.selectProductSort(elem.name);
-            if (elem.name === 'az' || elem.name === 'za') {
+            await inventoryPage.selectProductSort(elem.name[0]);
+
+            if (elem.name[0] === 'az') {
                 testData = await inventoryPage.getAllProductNames();
+                testParams[0].sort = [...testData].sort();
+
+            } else if (elem.name[0] === 'za') {
+                testData = await inventoryPage.getAllProductNames();
+                testParams[1].sort = [...testData].sort().reverse();
+
+            } else if (elem.name[0] === 'lohi') {
+                testData = await inventoryPage.getAllPrices();
+                testParams[2].sort = [...testData].sort((a, b) => a - b);
+
             } else {
                 testData = await inventoryPage.getAllPrices();
+                testParams[3].sort = [...testData].sort((a, b) => b - a);
+
             }
             expect(testData).toEqual(elem.sort);
         });
