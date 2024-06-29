b6be0f5e80685b9f48bde4365c3dc2b9f06015e6
SpockST <167228572+SpockST@users.noreply.github.com>
2024-06-29 16:37:17 +0300

    29.06

--- src/tests/performVerifySorting.spec.js
+++ src/tests/performVerifySorting.spec.js
@@ -4,10 +4,10 @@ const { login, password } = require('../auth/user.json');
 
 test.describe('Inventory Sorting', () => {
     const testParams = [
-        { name: ['az', 'Name'] },
-        { name: ['za', 'Name'] },
-        { name: ['lohi', 'Price'] },
-        { name: ['hilo', 'Price'] },
+        { sortingTitle: 'Title A to Z', sortingType: 'az', func: function(param) {return [...param].sort();}},
+        { sortingTitle: 'Title Z to A', sortingType:'za', func: function(param) {return [...param].sort().reverse();} },
+        { sortingTitle: 'PriceLow to High', sortingType: 'lohi', func: function(param) {return [...param].sort((a, b) => a - b);} },
+        { sortingTitle: 'Price High to Low', sortingType: 'hilo', func: function(param) {return [...param].sort((a, b) => b - a);} }
     ];
 
     test.beforeEach(async ({ inventoryPage, loginPage }) => {
@@ -16,28 +16,16 @@ test.describe('Inventory Sorting', () => {
     });
 
     testParams.forEach((elem) => {
-        test.only(`Sort Products by ${elem.name[1]} ${elem.name[0]}`, async ({ inventoryPage }) => {
+        test(`Sort Products by ${elem.sortingTitle}`, async ({ inventoryPage }) => {
+            await inventoryPage.selectProductSort(elem.sortingType);
+
             let testData = [];
-            await inventoryPage.selectProductSort(elem.name[0]);
-
-            if (elem.name[0] === 'az') {
+            if (elem.sortingType === 'az' || elem.sortingType === 'za') {
                 testData = await inventoryPage.getAllProductNames();
-                testParams[0].sort = [...testData].sort();
-
-            } else if (elem.name[0] === 'za') {
-                testData = await inventoryPage.getAllProductNames();
-                testParams[1].sort = [...testData].sort().reverse();
-
-            } else if (elem.name[0] === 'lohi') {
-                testData = await inventoryPage.getAllPrices();
-                testParams[2].sort = [...testData].sort((a, b) => a - b);
-
             } else {
                 testData = await inventoryPage.getAllPrices();
-                testParams[3].sort = [...testData].sort((a, b) => b - a);
-
             }
-            expect(testData).toEqual(elem.sort);
+            expect(testData).toEqual(elem.func(testData));
         });
     });
 });
