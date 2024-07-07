c6634d21d4d2f56bfdb7531b8e68f55083d790ba
SpockST <167228572+SpockST@users.noreply.github.com>
2024-07-07 17:06:24 +0300

    07.07.1

--- src/tests/nameDescriptionPriceCheckout.spec.js
+++ src/tests/nameDescriptionPriceCheckout.spec.js
@@ -14,34 +14,30 @@ test.describe('Checking the name, description and price of the product on the ch
         await shopingCartPage.navigate();
         await shopingCartPage.cheskout();
         await checkoutPage.informationInputAndContinue(USER.firstName, USER.lastName, USER.postalCode);
-
     });
 
-    test.only('Checking the product name on the checkout page', async ({ checkoutPage }) => {
+    test('Checking the product name on the checkout page', async ({ checkoutPage }) => {
         for (let key in testData.name) {
             let rez = await checkoutPage.geItemsNameById(key);
             expect(rez).toEqual(testData.name[key]);
         }
-        
     });
 
-    test.only('Checking the product description on the checkout page', async ({ checkoutPage }) => {
+    test('Checking the product description on the checkout page', async ({ checkoutPage }) => {
         for (let key in testData.description) {
             let rez = await checkoutPage.getItemsDescriptionById(key);
             expect(rez).toEqual(testData.description[key]);
-        }
-        
+        } 
     });
 
-    test.only('Checking the product prices on the checkout page', async ({ checkoutPage }) => {
+    test('Checking the product prices on the checkout page', async ({ checkoutPage }) => {
         for (let key in testData.prices) {
             let rez = await checkoutPage.getItemsPricesById(key);
             expect(rez).toEqual(testData.prices[key]);
-        }
-        
+        }    
     });
 
-    test.only('Checking the product totalPrice on the checkout page', async ({ checkoutPage }) => {
+    test('Checking the product totalPrice on the checkout page', async ({ checkoutPage }) => {
             let rez = await checkoutPage.getTotalPrice();
             console.log(rez);
             expect(rez).toEqual(testData.totalPrice);
